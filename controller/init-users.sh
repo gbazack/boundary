8144500f2045d63957c6478cd6046851911cc836
Edinio Zacko <morenzack@gmail.com>
2024-08-19 20:46:12 +0200

    (#1)update github actions and boundary controller

--- controller/init-users.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/sh
-boundary server -config /boundary/boundary.hcl
-boundary scopes create -name 'Main' -scope-id 'global' -recovery-config /boundary/boundary.hcl -skip-admin-role-creation -skip-default-role-creation
